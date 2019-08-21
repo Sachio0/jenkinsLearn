7fa18872719bf7d898c2f0ee197fce5183c7e614
Sachio0 <nowi.king@gmail.com>
2019-08-21 14:51:50 +0200

    create

---
+++ test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DoSth();
+        }
+        static void DoSth()
+        {
+            Console.WriteLine("do Sth");
+        }
+    }
+}
